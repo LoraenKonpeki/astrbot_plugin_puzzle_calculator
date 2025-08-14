f9b935133961540a8d0f57e5b38d8714505e81e2
Loraen_Konpeki <1521160808@qq.com>
2025-08-14 20:12:41 +0800

    修改旗语读取逻辑

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-import flag
 
 
 @register(
@@ -27,7 +26,7 @@ class MyPlugin(Star):
 
         /fl（或 /flag 或 /semaphore）+旗语
         数字对应方向参考电脑数字小键盘。（例如：2↓ 4← 8↑ 9↗）
-        为了输入速度，直接将所有两个一组的旗语拼起来输入。例如 "/fl 62797281"。
+        为了输入速度，建议直接将所有两个一组的旗语拼起来输入。例如 "/fl 62797281"。（当然也可以用空格分隔，如 "/fl 62 79 72 81"）
         不用担心输入顺序，28和82表示同一个旗语。
 
         如果你没有数字小键盘，可以使用键盘左半部分的 QWEDCXZA 八个方向作为输入。例如 "/fl dxQexqWZ"（不区分大小写）
@@ -37,140 +36,142 @@ class MyPlugin(Star):
         yield event.plain_result(help_message)
 
     @filter.command("flag", alias={"fl", "semaphore", "flagk", "flk", "semaphorek"})
-    async def flag_decode(self, event: AstrMessageEvent, flags_str: str):
+    async def flag_decode(self, event: AstrMessageEvent):
         """电脑键盘旗语解码"""
         message_str = event.message_str  # 用户发的纯文本消息字符串
         message_chain = event.get_messages()
         logger.info(message_chain)
         message_str = message_str.strip().split()
-        yield event.plain_result(flag.decode(message_str))
-        # flags = [flags_str[i : i + 2] for i in range(0, len(flags_str), 2)]
-        # if flags == []:
-        #     yield event.plain_result("请输入待解码旗语！")
-        # elif not all(len(flag) == 2 for flag in flags):
-        #     print(f"flags: {flags}")
-        #     yield event.plain_result(f"旗语必须是长度为2的字符串！\n你的输入：{flags}")
-        # else:
-        #     dic_flag = {
-        #         "12": "A",
-        #         "24": "B",
-        #         "27": "C",
-        #         "28": "D",
-        #         "29": "E",
-        #         "26": "F",
-        #         "23": "G",
-        #         "14": "H",
-        #         "71": "I",
-        #         "68": "J",
-        #         "81": "K",
-        #         "91": "L",
-        #         "16": "M",
-        #         "31": "N",
-        #         "74": "O",
-        #         "84": "P",
-        #         "94": "Q",
-        #         "64": "R",
-        #         "34": "S",
-        #         "78": "T",
-        #         "79": "U",
-        #         "83": "V",
-        #         "96": "W",
-        #         "93": "X",
-        #         "38": "Y",
-        #         "63": "Z",
-        #         "ZX": "A",
-        #         "XA": "B",
-        #         "XQ": "C",
-        #         "XW": "D",
-        #         "XE": "E",
-        #         "XD": "F",
-        #         "XC": "G",
-        #         "ZA": "H",
-        #         "QZ": "I",
-        #         "DW": "J",
-        #         "WZ": "K",
-        #         "EZ": "L",
-        #         "ZD": "M",
-        #         "CZ": "N",
-        #         "QA": "O",
-        #         "WA": "P",
-        #         "EA": "Q",
-        #         "DA": "R",
-        #         "CA": "S",
-        #         "QW": "T",
-        #         "QE": "U",
-        #         "WC": "V",
-        #         "ED": "W",
-        #         "EC": "X",
-        #         "CW": "Y",
-        #         "DC": "Z",
-        #     }
-        #     if "k" in message_str[0].lower():  # 检测是否使用手机键盘旗语
-        #         dic_flag = {
-        #             "78": "A",
-        #             "84": "B",
-        #             "81": "C",
-        #             "82": "D",
-        #             "83": "E",
-        #             "86": "F",
-        #             "89": "G",
-        #             "74": "H",
-        #             "17": "I",
-        #             "62": "J",
-        #             "27": "K",
-        #             "37": "L",
-        #             "76": "M",
-        #             "97": "N",
-        #             "14": "O",
-        #             "24": "P",
-        #             "34": "Q",
-        #             "64": "R",
-        #             "94": "S",
-        #             "12": "T",
-        #             "13": "U",
-        #             "29": "V",
-        #             "36": "W",
-        #             "39": "X",
-        #             "92": "Y",
-        #             "69": "Z",
-        #             "ZX": "A",
-        #             "XA": "B",
-        #             "XQ": "C",
-        #             "XW": "D",
-        #             "XE": "E",
-        #             "XD": "F",
-        #             "XC": "G",
-        #             "ZA": "H",
-        #             "QZ": "I",
-        #             "DW": "J",
-        #             "WZ": "K",
-        #             "EZ": "L",
-        #             "ZD": "M",
-        #             "CZ": "N",
-        #             "QA": "O",
-        #             "WA": "P",
-        #             "EA": "Q",
-        #             "DA": "R",
-        #             "CA": "S",
-        #             "QW": "T",
-        #             "QE": "U",
-        #             "WC": "V",
-        #             "ED": "W",
-        #             "EC": "X",
-        #             "CW": "Y",
-        #             "DC": "Z",
-        #         }
 
-        #     result = ""
-        #     for flag in flags:
-        #         flag = flag.upper()  # 转换为大写以匹配字典
-        #         if flag in dic_flag:
-        #             result += dic_flag[flag]
-        #         elif (flag[1] + flag[0]) in dic_flag:
-        #             result += dic_flag[flag[1] + flag[0]]
-        #         else:
-        #             result += "?"
-        #     yield event.plain_result(f"解码结果: \n{result.lower()}")
+        flags_strs = message_str[1:]  # 获取除指令外的所有内容
+        for flags_str in flags_strs:
+            flags += [flags_str[i : i + 2] for i in range(0, len(flags_str), 2)]
+        if flags == []:
+            yield event.plain_result("请输入待解码旗语！")
+        elif not all(len(flag) == 2 for flag in flags):
+            print(f"flags: {flags}")
+            yield event.plain_result(f"旗语必须是长度为2的字符串！\n你的输入：{flags}")
+        else:
+            dic_flag = {
+                "12": "A",
+                "24": "B",
+                "27": "C",
+                "28": "D",
+                "29": "E",
+                "26": "F",
+                "23": "G",
+                "14": "H",
+                "71": "I",
+                "68": "J",
+                "81": "K",
+                "91": "L",
+                "16": "M",
+                "31": "N",
+                "74": "O",
+                "84": "P",
+                "94": "Q",
+                "64": "R",
+                "34": "S",
+                "78": "T",
+                "79": "U",
+                "83": "V",
+                "96": "W",
+                "93": "X",
+                "38": "Y",
+                "63": "Z",
+                "ZX": "A",
+                "XA": "B",
+                "XQ": "C",
+                "XW": "D",
+                "XE": "E",
+                "XD": "F",
+                "XC": "G",
+                "ZA": "H",
+                "QZ": "I",
+                "DW": "J",
+                "WZ": "K",
+                "EZ": "L",
+                "ZD": "M",
+                "CZ": "N",
+                "QA": "O",
+                "WA": "P",
+                "EA": "Q",
+                "DA": "R",
+                "CA": "S",
+                "QW": "T",
+                "QE": "U",
+                "WC": "V",
+                "ED": "W",
+                "EC": "X",
+                "CW": "Y",
+                "DC": "Z",
+            }
+            if "k" in message_str[0].lower():  # 检测是否使用手机键盘旗语
+                dic_flag = {
+                    "78": "A",
+                    "84": "B",
+                    "81": "C",
+                    "82": "D",
+                    "83": "E",
+                    "86": "F",
+                    "89": "G",
+                    "74": "H",
+                    "17": "I",
+                    "62": "J",
+                    "27": "K",
+                    "37": "L",
+                    "76": "M",
+                    "97": "N",
+                    "14": "O",
+                    "24": "P",
+                    "34": "Q",
+                    "64": "R",
+                    "94": "S",
+                    "12": "T",
+                    "13": "U",
+                    "29": "V",
+                    "36": "W",
+                    "39": "X",
+                    "92": "Y",
+                    "69": "Z",
+                    "ZX": "A",
+                    "XA": "B",
+                    "XQ": "C",
+                    "XW": "D",
+                    "XE": "E",
+                    "XD": "F",
+                    "XC": "G",
+                    "ZA": "H",
+                    "QZ": "I",
+                    "DW": "J",
+                    "WZ": "K",
+                    "EZ": "L",
+                    "ZD": "M",
+                    "CZ": "N",
+                    "QA": "O",
+                    "WA": "P",
+                    "EA": "Q",
+                    "DA": "R",
+                    "CA": "S",
+                    "QW": "T",
+                    "QE": "U",
+                    "WC": "V",
+                    "ED": "W",
+                    "EC": "X",
+                    "CW": "Y",
+                    "DC": "Z",
+                }
+
+            result = ""
+            for flag in flags:
+                flag = flag.upper()  # 转换为大写以匹配字典
+                if flag in dic_flag:
+                    result += dic_flag[flag]
+                elif (flag[1] + flag[0]) in dic_flag:
+                    result += dic_flag[flag[1] + flag[0]]
+                else:
+                    result += "?"
+            yield event.plain_result(f"解码结果: \n{result.lower()}")
 
     @filter.command("blindhelp", alias={"blhelp"})
     async def blind_help(self, event: AstrMessageEvent):
